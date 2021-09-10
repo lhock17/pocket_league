b6407515354622abbe2688f451b9c2a2dc532602
Campbell McFadden <campbellmcfadden1@gmail.com>
2021-09-10 21:11:12 +1000

    Displaying background

--- main.asm
+++ main.asm
@@ -10,7 +10,11 @@
 ;--------------------------------------------------------
 	.globl _main
 	.globl _set_sprite_data
+	.globl _set_bkg_tiles
+	.globl _set_bkg_data
 	.globl _delay
+	.globl _TileLabel
+	.globl _map
 	.globl _car_light
 ;--------------------------------------------------------
 ; special function registers
@@ -43,14 +47,39 @@
 ; code
 ;--------------------------------------------------------
 	.area _CODE
-;main.c:5: void main(){
+;main.c:7: void main(){
 ;	---------------------------------
 ; Function main
 ; ---------------------------------
 _main::
-;main.c:6: UINT8 currentspriteindex = 0;
+;main.c:8: UINT8 currentspriteindex = 0;
 	ld	c, #0x00
-;main.c:8: set_sprite_data(0, 2, car_light);
+;main.c:11: set_bkg_data(0, 10, map);
+	ld	de, #_map
+	push	de
+	ld	hl, #0xa00
+	push	hl
+	call	_set_bkg_data
+	add	sp, #4
+;main.c:12: set_bkg_tiles(0, 0, 40, 18, TileLabel);
+	ld	de, #_TileLabel
+	push	de
+	ld	hl, #0x1228
+	push	hl
+	xor	a, a
+	rrca
+	push	af
+	call	_set_bkg_tiles
+	add	sp, #6
+;main.c:13: SHOW_BKG;
+	ldh	a, (_LCDC_REG + 0)
+	or	a, #0x01
+	ldh	(_LCDC_REG + 0), a
+;main.c:14: DISPLAY_ON;
+	ldh	a, (_LCDC_REG + 0)
+	or	a, #0x80
+	ldh	(_LCDC_REG + 0), a
+;main.c:16: set_sprite_data(0, 2, car_light);
 	ld	de, #_car_light
 	push	de
 	ld	hl, #0x200
@@ -66,17 +95,17 @@ _main::
 	ld	a, #0x4e
 	ld	(hl+), a
 	ld	(hl), #0x58
-;main.c:11: SHOW_SPRITES;
+;main.c:19: SHOW_SPRITES;
 	ldh	a, (_LCDC_REG + 0)
 	or	a, #0x02
 	ldh	(_LCDC_REG + 0), a
-;main.c:13: while(1){
+;main.c:21: while(1){
 00105$:
-;main.c:14: if(currentspriteindex==0){
+;main.c:22: if(currentspriteindex==0){
 	ld	a, c
 	or	a, a
-;main.c:15: currentspriteindex = 1;
-;main.c:18: currentspriteindex = 0;
+;main.c:23: currentspriteindex = 1;
+;main.c:26: currentspriteindex = 0;
 	ld	c, #0x01
 	jr	Z, 00103$
 	ld	c, #0x00
@@ -84,7 +113,7 @@ _main::
 ;C:/Users/campb/Documents/gbdk/include/gb/gb.h:1174: shadow_OAM[nb].tile=tile;
 	ld	hl, #(_shadow_OAM + 2)
 	ld	(hl), c
-;main.c:21: delay(1000);
+;main.c:29: delay(1000);
 	push	bc
 	ld	de, #0x03e8
 	push	de
@@ -100,8 +129,12 @@ _main::
 	ld	a, (de)
 	add	a, #0x0a
 	ld	(de), a
-;main.c:22: scroll_sprite(0,10,0);
-;main.c:24: }
+;C:/Users/campb/Documents/gbdk/include/gb/gb.h:860: SCX_REG+=x, SCY_REG+=y;
+	ldh	a, (_SCX_REG + 0)
+	inc	a
+	ldh	(_SCX_REG + 0), a
+;main.c:31: scroll_bkg(1,0);
+;main.c:33: }
 	jr	00105$
 _car_light:
 	.db #0x0f	; 15
@@ -168,6 +201,129 @@ _car_light:
 	.db #0xf0	; 240
 	.db #0xf0	; 240
 	.db #0xf0	; 240
+_map:
+	.db #0xff	; 255
+	.db #0xfa	; 250
+	.db #0x75	; 117	'u'
+	.db #0x55	; 85	'U'
+	.db #0x55	; 85	'U'
+	.db #0x55	; 85	'U'
+	.db #0xdc	; 220
+	.db #0x44	; 68	'D'
+	.db #0x44	; 68	'D'
+	.db #0x44	; 68	'D'
+	.db #0x43	; 67	'C'
+	.db #0x6f	; 111	'o'
+	.db #0xff	; 255
+	.db #0xff	; 255
+	.db #0xa8	; 168
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x98	; 152
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x46	; 70	'F'
+	.db #0xff	; 255
+	.db #0xfa	; 250
+	.db #0x81	; 129
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x98	; 152
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x04	; 4
+	.db #0x6f	; 111	'o'
+	.db #0xf9	; 249
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x98	; 152
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x5f	; 95
+	.db #0xfe	; 254
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x98	; 152
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x6f	; 111	'o'
+	.db #0xfe	; 254
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x98	; 152
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x6f	; 111	'o'
+	.db #0x40	; 64
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x98	; 152
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x6a	; 106	'j'
+	.db #0x31	; 49	'1'
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x11	; 17
+	.db #0x98	; 152
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x79	; 121	'y'
+	.db #0x31	; 49	'1'
+_TileLabel:
+	.db #0xaa	; 170
+	.db #0x00	; 0
+	.db #0x55	; 85	'U'
+	.db #0x00	; 0
+	.db #0xaa	; 170
+	.db #0x00	; 0
+	.db #0x55	; 85	'U'
+	.db #0x00	; 0
+	.db #0xaa	; 170
+	.db #0x00	; 0
+	.db #0x55	; 85	'U'
+	.db #0x00	; 0
+	.db #0xaa	; 170
+	.db #0x00	; 0
+	.db #0x55	; 85	'U'
+	.db #0x00	; 0
 	.area _CODE
 	.area _INITIALIZER
 	.area _CABS (ABS)
--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <gb/gb.h>
 #include <stdio.h>
 #include "car_light.c"
+#include "map.c"
+#include "map_sprites.c"
 
 void main(){
     UINT8 currentspriteindex = 0;
+
+    //background
+    set_bkg_data(0, 10, map);
+    set_bkg_tiles(0, 0, 40, 18, TileLabel);
+    SHOW_BKG;
+    DISPLAY_ON;
 
     set_sprite_data(0, 2, car_light);
     set_sprite_tile(0, 0);
@@ -20,5 +28,6 @@ void main(){
         set_sprite_tile(0, currentspriteindex);
         delay(1000);
         scroll_sprite(0,10,0);
+        //scroll_bkg(1,0);
     }
 }
