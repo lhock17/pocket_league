4c28e0d56f6153fdc42d8a895e750ef4cb1d1259
Campbell McFadden <campbellmcfadden1@gmail.com>
2021-09-11 08:40:27 +1000

    ball sprite

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "sprites/car_light.c"
 #include "sprites/map.c"
 #include "sprites/map_sprites.c"
+#include "sprites/ball1.c"
 
 #define AND &&
 #define OR ||
@@ -10,7 +11,17 @@
 #define SCREEN_HEIGHT 144;
 
 struct Car car1;
+struct GameObject ball;
 UBYTE spritesize = 8;
+
+struct GameObject {
+    UINT8 direction;
+    UBYTE spriteids[4];
+    UINT8 x;
+    UINT8 y;
+    UINT8 width;
+    UINT8 height;
+}
 
 struct Car {
 	UINT8 direction;
@@ -24,6 +35,17 @@ struct Car {
 UBYTE check_collisions(Car* one, Car* two) {
     return (one->x >= two->x AND one->x <= two-> x + two->width) OR
         (two->y >= one->y AND two->y <= one->y + one->height);
+}
+
+void load_ball_sprite(UINT8 direction) {
+    set_sprite_tile(0, 0);
+        ball.spriteids[0] = 0;
+        set_sprite_tile(1, 1);
+        ball.spriteids[1] = 1;
+        set_sprite_tile(2, 2);
+        ball.spriteids[2] = 2;
+        set_sprite_tile(3, 3);
+        ball.spriteids[3] = 3;
 }
 
 void load_car_sprite(UINT8 direction) {
@@ -51,6 +73,13 @@ void performantdelay(UINT8 numloops){
     }     
 }
 
+void setup_ball() {
+    ball.x = 30;
+    ball.y = 0;
+    ball.width = 16;
+    ball.height = 16;   
+}
+
 void setupcar_light(){
     car1.direction = 0;
     car1.x = 80;
@@ -60,17 +89,6 @@ void setupcar_light(){
 
     set_sprite_data(0, 8, car_light);
     load_car_sprite(car1.direction);
-
-    // // load sprites for car
-    // //background
-    // set_bkg_data(0, 10, map);
-    // set_bkg_tiles(0, 0, 40, 18, TileLabel);
-
-    // //background
-    // set_bkg_data(0, 50, map);
-    // //set_bkg_tiles(0, 0, 40, 18, TileLabel);
-    // SHOW_BKG;
-    // DISPLAY_ON;
     
     movegamecharacter(&car1, car1.x, car1.y);
 }
@@ -85,6 +103,8 @@ void main(){
 
     set_sprite_data(0,4, car_light);
     setupcar_light();
+    setup_ball();
+
     SHOW_SPRITES;
     DISPLAY_ON;
 
